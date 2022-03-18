7386079d01b88fc555c0cc1f425b84d251dbe21d
EthemCuhadar <iethemcuhadar@gmail.com>
2022-03-18 16:36:55 +0300

    The user can get information about a book by typing ID

---
+++ cmd/get.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"fmt"
+	"bookstore/data"
+)
+
+// GetBook - Allows the user to see information about a book by entering
+// the book ID. The function returns boolean. If the ID which are typed by
+// user doesnt match in database, the function will return false. Otherwise,
+// the user can see all information (relative fields) about the book.
+func GetBook(s []string)bool{
+	for _, b := range data.BookDatabase.Items{
+		if b.ID == s[2]{
+			fmt.Println("--------------- Related Book ----------------")
+			b.ShowInfo()
+			return true
+		}
+	}
+	fmt.Println("Book ID does not exist in database.")
+	return false
+}
